5daa8d9d198c72fa9983f7f306f6d5e1de6aa7ec
Boloelo <awpxdhead@gmail.com>
2024-10-11 00:15:20 +0200

    zrobione

--- kwartaly/kwartaly.js
+++ kwartaly/kwartaly.js
@@ -1,15 +1,29 @@
-const guzik = document.getElementById('guzik');
-const liczba = document.getElementById('liczba');
-const miesiac = Math.floor(Math.random() * 15) + 1;
-const kwartal = document.getElementById('kwartal');
 
-guzik.addEventListener("click", function () {
-
-liczba.innerHTML=miesiac;
-
-switch(miesiac = 1,2,3){
-    kwartal.innerHTML='I kwartal';
-    
+let miesiac = Math.floor(Math.random() * 15) + 1;
+let kwartał = "";
+switch (miesiac) {
+    case 1:
+    case 2:
+    case 3:
+        kwartał = "I kwartał";
+        break;
+    case 4:
+    case 5:
+    case 6:
+        kwartał = "II kwartał";
+        break;
+    case 7:
+    case 8:
+    case 9:
+        kwartał = "III kwartał";
+        break;
+    case 10:
+    case 11:
+    case 12:
+        kwartał = "IV kwartał";
+        break;
+    default:
+        kwartał = "Błędny numer miesiąca";
 }
-
-})
+document.getElementById("miesiac").innerHTML = miesiac;
+document.getElementById("kwartal").innerHTML = kwartał;
